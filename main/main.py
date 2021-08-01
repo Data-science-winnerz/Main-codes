bd21a292ca53ac113b081f1cd494eed2162d5f6d
Appu13 <vishalvinaydas@gmail.com>
2021-08-01 18:51:10 +0530

    Updated heaer function for s3.pdf

--- main/main.py
+++ main/main.py
@@ -3,15 +3,16 @@ from preprocess import *
 from pngcon import convo
 from table_extract import clean_extract
 from spliting import head_extract
+from spliting_pdf2 import head_extract2
 from placing import arrange_dump
 import numpy as np
 import cv2
 
 
 
-img = convo(r"E:\Projects\Data winnerz ocr\Images\sample.pdf")
+img = convo(r"C:\Users\Vishal\Desktop\Main-codes\Images\s3.pdf")
 opencvImage = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
-print("Image converted successfully")
+print("Image conversion successful")
 
 # Getting the ocr
 txt = ocr(noise_removal(opencvImage))
@@ -20,9 +21,15 @@ print("ocrd successfully")
 table = clean_extract(txt=txt)
 print("Table extracted")
 
+# For pdf 1
+# h = table.pop(0)
+# headers = head_extract(h)
+# print('headers modified')
+
+# For pdf 2
 h = table.pop(0)
-headers = head_extract(h)
+headers = head_extract2(h)
 print('headers modified')
 
-arrange_dump(table,headers)
-print('JSON created')
+# arrange_dump(table,headers)
+# print('JSON created')
